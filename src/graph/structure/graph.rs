5aacf0e73cced53d14f7d6649f461432792b5f4b
celdir <michael.earl7769@gmail.com>
2022-07-28 12:14:45 -0700

    Add traits: Graph, DirectedGraph, MultiGraph, MapGraph

--- src/graph/structure/graph.rs
+++ src/graph/structure/graph.rs
@@ -1,75 +1,80 @@
-/*use std::iter::Iterator;
-use std::hash::Hash;
+use crate::graph::structure::edge::Edge;
+use crate::graph::structure::node::Node;
 
 pub trait Graph<'a> {
-    type Node: 'a;
-    type Edge: 'a;
-    type NodeId: Copy + Eq + Hash;
-    type EdgeId: Copy + Eq + Hash;
+    type N: 'a;
+    type NId;
+    type E: 'a;
+    type EId;
 
-    fn nodes(&self) -> Box<dyn Iterator<Item = (Self::NodeId, &Self::Node)> + '_>;
-    fn edges(&self) -> Box<dyn Iterator<Item = (Self::NodeId, Self::NodeId, &Self::Edge)> + '_>;
+    type NodeIterator: Iterator<Item = Node<'a, Self::NId, Self::N>>;
+    type EdgeIterator: Iterator<Item = Edge<'a, Self::NId, Self::EId, Self::E>>;
+    type AdjIterator: Iterator<
+        Item = (
+            Edge<'a, Self::NId, Self::EId, Self::E>,
+            Node<'a, Self::NId, Self::N>,
+        ),
+    >;
 
-    fn adj(
-        &self,
-        u: Self::NodeId,
-    ) -> Box<dyn Iterator<Item = (Self::NodeId, Self::NodeId, &Self::Edge)> + '_>;
+    fn len(&self) -> (usize, usize);
 
-    fn nodes_len(&self) -> usize;
-    fn edges_len(&self) -> usize;
+    fn contains_node(&self, id: Self::NId) -> bool;
+    fn node(&self, id: Self::NId) -> Option<Node<Self::NId, Self::N>>;
+    fn node_data(&self, id: Self::NId) -> Option<&Self::N>;
+    fn node_data_mut(&mut self, id: Self::NId) -> Option<&mut Self::N>;
+    fn degree(&self, u: Self::NId) -> usize;
+    fn insert_node(&mut self, node: Self::N) -> Self::NId;
+    fn remove_node(&mut self, id: Self::NId) -> Option<Self::N>;
+    fn clear_node(&mut self, id: Self::NId) -> Option<()>;
 
-    fn get_node(&self, u: Self::NodeId) -> Option<&Self::Node>;
-    fn get_edge(&self, u: Self::NodeId, v: Self::NodeId) -> Option<&Self::Edge>;
+    fn contains_edge(&self, u: Self::NId, v: Self::NId) -> bool;
+    fn edge(&self, id: Self::EId) -> Option<Edge<Self::EId, Self::EId, Self::E>>;
+    fn between(&self, u: Self::NId, v: Self::NId) -> Option<Edge<Self::NId, Self::EId, Self::E>>;
+    fn edge_data(&self, id: Self::EId) -> Option<&Self::E>;
+    fn edge_data_mut(&mut self, id: Self::EId) -> Option<&mut Self::E>;
+    fn insert_edge(&mut self, u: Self::NId, v: Self::NId, edge: Self::E) -> Option<Self::EId>;
+    fn remove_edge(&mut self, id: Self::EId) -> Option<Self::E>;
 
-    fn insert_node(&mut self, node: Self::Node) -> Self::NodeId;
-    fn insert_edge(&mut self, u: Self::NodeId, v: Self::NodeId, edge: Self::Edge);
+    fn nodes(&self) -> Self::NodeIterator;
+    fn edges(&self) -> Self::EdgeIterator;
+    // Returns out edges for directed graph or all edges for undirected
+    fn adj(&self, u: Self::NId) -> Option<Self::AdjIterator>;
+}
 
-    fn remove_node(&mut self, id: Self::NodeId) -> Self::Node;
-    fn remove_edge(&mut self, id: Self::EdgeId) -> Self::Edge;
+pub trait DirectedGraph<'a> {
+    type N: 'a;
+    type NId;
+    type E: 'a;
+    type EId;
+
+    type AdjIterator: Iterator<
+        Item = (
+            Edge<'a, Self::NId, Self::EId, Self::E>,
+            Node<'a, Self::NId, Self::N>,
+        ),
+    >;
+
+    fn out_edges(&self, u: Self::NId) -> Option<Self::AdjIterator>;
+    fn out_degree(&self, u: Self::NId) -> usize;
+
+    fn in_edges(&self, u: Self::NId) -> Option<Self::AdjIterator>;
+    fn in_degree(&self, u: Self::NId) -> usize;
+}
+
+pub trait MultiGraph<'a> {
+    type N: 'a;
+    type NId;
+    type E: 'a;
+    type EId;
+
+    type EdgeIterator: Iterator<Item = Edge<'a, Self::NId, Self::EId, Self::E>>;
+
+    fn between_multi(&self, u: Self::NId, v: Self::NId) -> Option<Self::EdgeIterator>;
 }
 
 pub trait MapGraph<'a> {
-    fn insert_node(&mut self, id: Self::NodeId, node: Self::Node) -> Self::NodeId;
-}*/
-/*
-pub trait IterableGraph {
-    type N;
-    type NId = usize;
-    type E;
-    type EId;
+    type N: 'a;
+    type NId;
 
-    fn nodes(&self) -> Box<dyn Iterator<Item = Node<Self::NId, Self::N>> + '_>;
-
-    fn nodes_len(&self) -> usize;
-
-    fn get_node(&self, u: Self::NId) -> Option<Node<Self::NId, Self::N>>;
-
-    fn edges(&self) -> Box<dyn Iterator<Item = Edge<Self::NId, Self::EId, Self::E>> + '_>;
-
-    fn edges_len(&self) -> usize;
-
-    fn get_edge(&self, u: Self::NId, v: Self::NId) -> Option<Edge<Self::NId, Self::EId, Self::E>>;
-
-    fn adj_edges(
-        &self,
-        u: Self::NId,
-    ) -> Box<dyn Iterator<Item = Edge<Self::NId, Self::EId, Self::E>> + '_>;
+    fn put_node(&mut self, id: Self::NId, node: Self::N) -> Option<Self::N>;
 }
-
-pub trait MutableGraph {
-    fn insert_node(&mut self, node: Self::N) -> Self::Id;
-
-    fn put_node(&mut self, id: Self::Id, node: Self::N) -> Option<Self::N>; // returns old node data if present
-
-    fn remove_node(&mut self, id: Self::Id) -> Option<Self::N>;
-
-    fn insert_edge(&mut self, u: Self::NId, v: Self::NId, edge: Self::E) -> Self::EId;
-
-    fn put_edge(&mut self, id: Self::EId, u: Self::NId, v: Self::NId, edge: Self::E) -> Option<Self::E>; // returns old edge data if present
-
-    fn remove_edge(&mut self, id: Self::EId) -> Option<Self::E>;
-}
-
-pub trait MapGraph {}
-pub trait Directed {}
-pub trait Undirected {}*/
