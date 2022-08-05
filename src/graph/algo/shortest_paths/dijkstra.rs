297c7eb9ab87eb41f19e41cb57766df01f26268b
Celdir <michael.earl7769@gmail.com>
2022-08-05 12:28:29 -0700

    add more unit test for dijkstra

--- src/graph/algo/shortest_paths/dijkstra.rs
+++ src/graph/algo/shortest_paths/dijkstra.rs
@@ -55,6 +55,7 @@ where
 mod tests {
     use crate::graph::algo::shortest_paths::dijkstra::dijkstra;
     use crate::graph::structure::mapgraph::MapGraph;
+    use crate::graph::structure::vecgraph::StableVecGraph;
 
     #[test]
     fn dijkstra_base_case() {
@@ -88,5 +89,55 @@ mod tests {
         let path_to_b = tree.path("B");
         let ids: Vec<_> = path_to_b.iter().map(|edge| edge.origin()).collect();
         assert_eq!(ids, vec!["A", "C", "D"]);
+    }
+
+    #[test]
+    fn dijkstra_directed_base_case() {
+        //     > B---\
+        //    /  ^    1
+        //   5   4     \
+        //  /    |      v
+        // A -3->C -6-> E
+        //  \    ^      ^
+        //   1   1     /
+        //    \  |    7   
+        //     > D---/
+        //  legend: A-E are actually 0-4 node ids
+        let graph = StableVecGraph::from((
+            vec![(); 5],
+            vec![(0, 1, 5), (0, 2, 3), (0, 3, 1), (2, 1, 4), (3, 2, 1), (1, 4, 1), (3, 4, 7), (2, 4, 6)],
+        ));
+
+        let tree = dijkstra(&graph, 0);
+        assert_eq!(tree.dist(0), Some(&0));
+        assert_eq!(tree.dist(1), Some(&5));
+        assert_eq!(tree.dist(2), Some(&2));
+        assert_eq!(tree.dist(3), Some(&1));
+        assert_eq!(tree.dist(4), Some(&6));
+
+        assert!(tree.parent_edge(0).is_none());
+        let b_edge = tree.parent_edge(1).unwrap();
+        assert_eq!(b_edge.origin(), 0);
+        assert_eq!(b_edge.data(), &5);
+        let c_edge = tree.parent_edge(2).unwrap();
+        assert_eq!(c_edge.origin(), 3);
+        assert_eq!(c_edge.data(), &1);
+        let d_edge = tree.parent_edge(3).unwrap();
+        assert_eq!(d_edge.origin(), 0);
+        assert_eq!(d_edge.data(), &1);
+        let e_edge = tree.parent_edge(4).unwrap();
+        assert_eq!(e_edge.origin(), 1);
+        assert_eq!(e_edge.data(), &1);
+
+        let path_to_e = tree.path(4);
+        let ids: Vec<_> = path_to_e.iter().map(|edge| edge.origin()).collect();
+        assert_eq!(ids, vec![0, 1]);
+
+        let empty_tree = dijkstra(&graph, 4);
+        assert_eq!(empty_tree.dist(0), None);
+        assert_eq!(empty_tree.dist(1), None);
+        assert_eq!(empty_tree.dist(2), None);
+        assert_eq!(empty_tree.dist(3), None);
+        assert_eq!(empty_tree.dist(4), Some(&0));
     }
 }
