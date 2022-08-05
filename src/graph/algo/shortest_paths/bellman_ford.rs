7ec08197a08814375ac8b682d13fa4983833596b
Celdir <michael.earl7769@gmail.com>
2022-08-05 15:29:11 -0700

    Bellman ford impl + unit tests

--- src/graph/algo/shortest_paths/bellman_ford.rs
+++ src/graph/algo/shortest_paths/bellman_ford.rs
@@ -2,7 +2,6 @@ use crate::graph::algo::shortest_paths::shortest_path_tree::ShortestPathTree;
 use crate::graph::structure::edge::Edge;
 use crate::graph::structure::graph::Graph;
 use crate::graph::structure::node::Node;
-use priority_queue::PriorityQueue;
 use std::cmp::Ord;
 use std::cmp::Ordering;
 use std::cmp::Reverse;
@@ -10,7 +9,8 @@ use std::collections::HashMap;
 use std::default::Default;
 use std::ops::Add;
 
-pub fn dijkstra<'a, G>(graph: &'a G, start: G::NId) -> ShortestPathTree<'a, G>
+// returns shortest path tree and boolean that is true if negative cycle is found
+pub fn bellman_ford<'a, G>(graph: &'a G, start: G::NId) -> (ShortestPathTree<'a, G>, bool)
 where
     G: Graph<'a>,
     G::E: Add<Output = G::E> + Ord + Default + Clone,
@@ -19,46 +19,51 @@ where
     let mut parent = HashMap::new();
 
     if !graph.contains_node(start) {
-        return ShortestPathTree::new(dist, parent);
+        return (ShortestPathTree::new(dist, parent), false);
     }
 
-    // initialize min heap
-    let mut pq: PriorityQueue<G::NId, Reverse<G::E>> = PriorityQueue::new();
-    pq.push(start, Reverse(G::E::default()));
+    dist.insert(start, G::E::default());
 
-    while let Some((id, Reverse(cost))) = pq.pop() {
-        dist.insert(id, cost.clone());
+    // n-1 iterations to find shortest paths, +1 final iteration to check for negative cycle
+    let (iterations, _) = graph.len();
+    let mut change = true;
+    for _ in 0..iterations {
+        if !change {
+            break;
+        }
+        change = false;
 
-        for (edge, node) in graph.adj(id).unwrap() {
-            let next_cost = cost.clone() + edge.data().clone();
-            let nid = node.id();
-            if !dist.contains_key(&nid) {
-                // "push_increase" actually decreases the cost if possible because Reverse
-                let priority = Reverse(next_cost.clone());
-                let old_priority = pq.push_increase(nid, priority);
+        for u in graph.nodes() {
+            if !dist.contains_key(&u.id()) {
+                continue;
+            }
+            for (edge, v) in graph.adj(u.id()).unwrap() {
+                let v_id = v.id();
+                let weight = edge.data().clone();
 
-                // update parent if next_cost is less than old_cost or old_cost doesn't exist
-                match old_priority {
-                    Some(Reverse(old_cost)) if old_cost <= next_cost => {}
-                    _ => {
-                        parent.insert(nid, edge);
-                    }
+                let u_dist = dist[&u.id()].clone();
+                let v_dist_new = u_dist + weight;
+
+                if !dist.contains_key(&v_id) || v_dist_new < dist[&v_id] {
+                    dist.insert(v_id, v_dist_new);
+                    parent.insert(v_id, edge);
+                    change = true;
                 }
             }
         }
     }
 
-    ShortestPathTree::new(dist, parent)
+    (ShortestPathTree::new(dist, parent), change)
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::graph::algo::shortest_paths::dijkstra::dijkstra;
+    use crate::graph::algo::shortest_paths::bellman_ford::bellman_ford;
     use crate::graph::structure::mapgraph::MapGraph;
     use crate::graph::structure::vecgraph::StableVecGraph;
 
     #[test]
-    fn dijkstra_base_case() {
+    fn bellman_ford_base_case() {
         // A --5-- B
         // |       |
         // 2       1
@@ -69,7 +74,7 @@ mod tests {
             vec![("A", "B", 5), ("A", "C", 2), ("C", "D", 1), ("B", "D", 1)],
         ));
 
-        let tree = dijkstra(&graph, "A");
+        let (tree, _) = bellman_ford(&graph, "A");
         assert_eq!(tree.dist("A"), Some(&0));
         assert_eq!(tree.dist("B"), Some(&4));
         assert_eq!(tree.dist("C"), Some(&2));
@@ -92,7 +97,7 @@ mod tests {
     }
 
     #[test]
-    fn dijkstra_directed_base_case() {
+    fn bellman_ford_directed_base_case() {
         //     > B---\
         //    /  ^    1
         //   5   4     \
@@ -117,7 +122,7 @@ mod tests {
             ],
         ));
 
-        let tree = dijkstra(&graph, 0);
+        let (tree, _) = bellman_ford(&graph, 0);
         assert_eq!(tree.dist(0), Some(&0));
         assert_eq!(tree.dist(1), Some(&5));
         assert_eq!(tree.dist(2), Some(&2));
@@ -142,11 +147,63 @@ mod tests {
         let ids: Vec<_> = path_to_e.iter().map(|edge| edge.origin()).collect();
         assert_eq!(ids, vec![0, 1]);
 
-        let empty_tree = dijkstra(&graph, 4);
+        let (empty_tree, _) = bellman_ford(&graph, 4);
         assert_eq!(empty_tree.dist(0), None);
         assert_eq!(empty_tree.dist(1), None);
         assert_eq!(empty_tree.dist(2), None);
         assert_eq!(empty_tree.dist(3), None);
         assert_eq!(empty_tree.dist(4), Some(&0));
+    }
+
+    #[test]
+    fn bellman_ford_negative_cycle() {
+        // A --5-- B
+        // |       |
+        // 2       -6
+        // |       |
+        // C -(-2)-D
+        let graph = MapGraph::from((
+            vec![("A", ()), ("B", ()), ("C", ()), ("D", ())],
+            vec![("A", "B", 5), ("A", "C", 2), ("C", "D", -2), ("B", "D", -6)],
+        ));
+
+        let (tree, has_cycle) = bellman_ford(&graph, "A");
+        assert!(has_cycle);
+    }
+
+    #[test]
+    fn bellman_ford_negative_cycle_of_2() {
+        // A --5-- B
+        // |       |
+        // 2       -4
+        // |       |
+        // C -(-2)-D
+        let graph = MapGraph::from((
+            vec![("A", ()), ("B", ()), ("C", ()), ("D", ())],
+            vec![("A", "B", 5), ("A", "C", 2), ("C", "D", -2), ("B", "D", -4)],
+        ));
+
+        let (tree, has_cycle) = bellman_ford(&graph, "A");
+        assert!(has_cycle);
+    }
+
+    #[test]
+    fn bellman_ford_negative_no_cycle() {
+        // A-(-5)-> B
+        // |        |
+        //-2       -6
+        // |        v
+        // C-(-2)-> D
+        let graph = StableVecGraph::from((
+            vec![(); 4],
+            vec![(0, 1, -5), (0, 2, -2), (2, 3, -2), (1, 3, -6)],
+        ));
+
+        let (tree, has_cycle) = bellman_ford(&graph, 0);
+        assert!(!has_cycle);
+        assert_eq!(tree.dist(0), Some(&0));
+        assert_eq!(tree.dist(1), Some(&-5));
+        assert_eq!(tree.dist(2), Some(&-2));
+        assert_eq!(tree.dist(3), Some(&-11));
     }
 }
--- src/graph/algo/shortest_paths/mod.rs
+++ src/graph/algo/shortest_paths/mod.rs
@@ -1,5 +1,6 @@
 pub mod shortest_path_tree;
 pub mod dijkstra;
+pub mod bellman_ford;
 
 //pub use self::shortest_path_tree;
 //pub use self::dijkstra;
