ca60397f65d7fd7cdd8e1e47abe9c67caf2ac516
Celdir <michael.earl7769@gmail.com>
2022-09-11 22:40:28 -0700

    astar unit test

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
- TODO
+TODO
  - Reconsier all of the Option<> return values in functions that take node and edge ids. It might be better to panic on invalid ids instead, and document "valid ids" as a precondition of using the library
+
+Future functionality:
+- Max flow
+- Graph visualization
--- src/graph/algo/shortest_paths/astar.rs
+++ src/graph/algo/shortest_paths/astar.rs
@@ -52,103 +52,41 @@ where
     ShortestPathTree::new(dist, parent).path(end)
 }
 
-/*
 #[cfg(test)]
 mod tests {
     use crate::graph::algo::shortest_paths::astar::astar;
+    use crate::graph::structure::graph::{Graph, KeyedGraph};
     use crate::graph::structure::mapgraph::MapGraph;
-    use crate::graph::structure::vecgraph::StableVecGraph;
 
     #[test]
-    fn astar_base_case() {
-        // A --5-- B
-        // |       |
-        // 2       1
-        // |       |
-        // C --1-- D
-        let graph = MapGraph::from((
-            vec![("A", ()), ("B", ()), ("C", ()), ("D", ())],
-            vec![("A", "B", 5), ("A", "C", 2), ("C", "D", 1), ("B", "D", 1)],
-        ));
+    fn astar_grid() {
+        let mut graph = MapGraph::<(usize, usize), (), i64>::new();
 
-        let tree = astar(&graph, "A");
-        assert_eq!(tree.dist("A"), Some(&0));
-        assert_eq!(tree.dist("B"), Some(&4));
-        assert_eq!(tree.dist("C"), Some(&2));
-        assert_eq!(tree.dist("D"), Some(&3));
+        for x in 0..101 {
+            for y in 0..101 {
+                graph.put_node((x, y), ());
+            }
+        }
 
-        assert!(tree.parent_edge("A").is_none());
-        let b_edge = tree.parent_edge("B").unwrap();
-        assert_eq!(b_edge.other("B"), "D");
-        assert_eq!(b_edge.data(), &1);
-        let c_edge = tree.parent_edge("C").unwrap();
-        assert_eq!(c_edge.other("C"), "A");
-        assert_eq!(c_edge.data(), &2);
-        let d_edge = tree.parent_edge("D").unwrap();
-        assert_eq!(d_edge.other("D"), "C");
-        assert_eq!(d_edge.data(), &1);
+        let deltas = vec![(0, 1), (1, 0)];
 
-        let path_to_b = tree.path("B");
-        let ids: Vec<_> = path_to_b.iter().map(|edge| edge.origin()).collect();
-        assert_eq!(ids, vec!["A", "C", "D"]);
+        for x in 0..101 {
+            for y in 0..101 {
+                for d in &deltas {
+                    let nx = x + d.0;
+                    let ny = y + d.1;
+                    if nx < 101 && ny < 101 {
+                        graph.insert_edge((x, y), (nx, ny), 1);
+                    }
+                }
+            }
+        }
+
+        let path = astar(&graph, (0, 0), (100, 100), |node| {
+            let (x, y) = node.id();
+            (((100 - x).pow(2) + (100 - y).pow(2)) as f64).sqrt().floor() as i64
+        }).expect("path should exist");
+
+        assert_eq!(path.dist, 200);
     }
-
-    #[test]
-    fn astar_directed_base_case() {
-        //     > B---\
-        //    /  ^    1
-        //   5   4     \
-        //  /    |      v
-        // A -3->C -6-> E
-        //  \    ^      ^
-        //   1   1     /
-        //    \  |    7
-        //     > D---/
-        //  legend: A-E are actually 0-4 node ids
-        let graph = StableVecGraph::from((
-            vec![(); 5],
-            vec![
-                (0, 1, 5),
-                (0, 2, 3),
-                (0, 3, 1),
-                (2, 1, 4),
-                (3, 2, 1),
-                (1, 4, 1),
-                (3, 4, 7),
-                (2, 4, 6),
-            ],
-        ));
-
-        let tree = astar(&graph, 0);
-        assert_eq!(tree.dist(0), Some(&0));
-        assert_eq!(tree.dist(1), Some(&5));
-        assert_eq!(tree.dist(2), Some(&2));
-        assert_eq!(tree.dist(3), Some(&1));
-        assert_eq!(tree.dist(4), Some(&6));
-
-        assert!(tree.parent_edge(0).is_none());
-        let b_edge = tree.parent_edge(1).unwrap();
-        assert_eq!(b_edge.origin(), 0);
-        assert_eq!(b_edge.data(), &5);
-        let c_edge = tree.parent_edge(2).unwrap();
-        assert_eq!(c_edge.origin(), 3);
-        assert_eq!(c_edge.data(), &1);
-        let d_edge = tree.parent_edge(3).unwrap();
-        assert_eq!(d_edge.origin(), 0);
-        assert_eq!(d_edge.data(), &1);
-        let e_edge = tree.parent_edge(4).unwrap();
-        assert_eq!(e_edge.origin(), 1);
-        assert_eq!(e_edge.data(), &1);
-
-        let path_to_e = tree.path(4);
-        let ids: Vec<_> = path_to_e.iter().map(|edge| edge.origin()).collect();
-        assert_eq!(ids, vec![0, 1]);
-
-        let empty_tree = astar(&graph, 4);
-        assert_eq!(empty_tree.dist(0), None);
-        assert_eq!(empty_tree.dist(1), None);
-        assert_eq!(empty_tree.dist(2), None);
-        assert_eq!(empty_tree.dist(3), None);
-        assert_eq!(empty_tree.dist(4), Some(&0));
-    }
-}*/
+}
