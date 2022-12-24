f1c205a8c50d763ad37e4f122a81aff254eff1d5
celdir <michael.earl7769@gmail.com>
2022-12-23 17:56:39 -0800

    Fix bug in DFS and add tests

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
 TODO
- - Tests for DFS
  - Reconsider all of the Option<> return values in functions that take node and edge ids. It might be better to panic on invalid ids instead, and document "valid ids" as a precondition of using the library
  - For containers like Vec<Node>, mark with an Ordinal marker trait. When AdjContainer is Ordinal, require NodeContainer to be ordinal also
  - Containers that are not Ordinal can be marked with Keyed
  - Idea for graph impl: Store edges in single list sorted by (u, v). Adjacency is tracked by storing for each node the start index of edges for that node. The DiGraph version can have a separate list for in edges. Insertion / removal is O(N+E), but iteration is very fast since it's just one vec. Call it FlatAdjList / FlatGraph
  - For containers, panic on invalid input. Invariants should be enforced at higher level (in CGraph). Clean up container contracts / spell out behavior assumptions explicitly. e.g. adj container `insert_node` has no return value. it should either return Option<> to denote error or panic on any invalid input
- - Change old unit tests to use CGraph types and then deprecate old VecGraph and MapGraph
  - Max flow:
     - Create FlowGraph struct that holds G: DirectedGraph and implements Graph, but does custom logic for managing back edges and such, and provides special methods like `back_edges()`.
     - G::E must be type Flow, which is a struct holding some integer type storing capacity and flow and provides a method for `residual()`, or `cap - flow`
--- src/iter/dfs.rs
+++ src/iter/dfs.rs
@@ -1,6 +1,6 @@
 use crate::graph::edge::Edge;
-use crate::graph::traits::Graph;
 use crate::graph::node::Node;
+use crate::graph::traits::Graph;
 use std::collections::HashMap;
 
 pub fn dfs<'a, G: Graph>(graph: &'a G, start: G::NId) -> Dfs<'a, G> {
@@ -9,7 +9,7 @@ pub fn dfs<'a, G: Graph>(graph: &'a G, start: G::NId) -> Dfs<'a, G> {
 
 pub struct Dfs<'a, G: Graph> {
     graph: &'a G,
-    stack: Vec<G::NId>,
+    stack: Vec<(G::NId, Option<G::EId>)>,
     parent: HashMap<G::NId, Option<G::EId>>,
 }
 
@@ -20,26 +20,23 @@ impl<'a, G: Graph> Iterator for Dfs<'a, G> {
     );
 
     fn next(&mut self) -> Option<Self::Item> {
-        let node_id = self.stack.pop()?;
-        if !self.parent.contains_key(&node_id) {
-            self.parent.insert(node_id, None);
+        let (mut node_id, mut edge_id_opt) = self.stack.pop()?;
+        while self.parent.contains_key(&node_id) {
+            (node_id, edge_id_opt) = self.stack.pop()?;
         }
+
+        self.parent.insert(node_id, edge_id_opt);
 
         let adj: Vec<_> = self.graph.adj(node_id)?.collect();
         for (edge, node) in adj.iter().rev() {
             let next_id = node.id();
             if !self.parent.contains_key(&next_id) {
-                self.parent.insert(next_id, Some(edge.id()));
-                self.stack.push(next_id);
+                self.stack.push((next_id, Some(edge.id())));
             }
         }
 
         let node = self.graph.node(node_id).unwrap();
-        let parent_edge_opt = self
-            .parent
-            .get(&node_id)
-            .unwrap()
-            .map(|edge_id| self.graph.edge(edge_id).unwrap());
+        let parent_edge_opt = edge_id_opt.map(|edge_id| self.graph.edge(edge_id).unwrap());
         Some((parent_edge_opt, node))
     }
 }
@@ -48,8 +45,92 @@ impl<'a, G: Graph> Dfs<'a, G> {
     fn new(graph: &'a G, start: G::NId) -> Dfs<'a, G> {
         Dfs {
             graph,
-            stack: vec![start],
+            stack: vec![(start, None)],
             parent: HashMap::new(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::graph::traits::{Graph, OrdinalGraph, WithCapacity};
+    use crate::graph::types::{DiListGraph, UnListGraph};
+    use crate::iter::dfs::dfs;
+    use std::collections::HashMap;
+
+    #[test]
+    fn dfs_digraph() {
+        let graph = DiListGraph::from_ordinal(
+            vec![(); 7],
+            vec![
+                (0, 1, ()),
+                (0, 2, ()),
+                (1, 3, ()),
+                (1, 4, ()),
+                (2, 5, ()),
+                (2, 6, ()),
+                (6, 0, ()),
+                (5, 2, ()),
+                (5, 6, ()),
+                (3, 2, ()),
+                (5, 1, ()),
+            ],
+        );
+
+        let expected_parents = HashMap::from([(1, 0), (2, 3), (3, 1), (4, 1), (5, 2), (6, 5)]);
+        for (parent_edge, node) in dfs(&graph, 0) {
+            match parent_edge {
+                Some(edge) => {
+                    let id = node.id();
+                    let expected_parent_id = *expected_parents
+                        .get(&id)
+                        .expect("expected parents map should contain node id");
+                    assert_eq!(
+                        edge.other(id),
+                        expected_parent_id,
+                        "parent of {} should be {} but is {}",
+                        id,
+                        expected_parent_id,
+                        edge.other(id)
+                    )
+                }
+                _ => assert!(!expected_parents.contains_key(&node.id())),
+            }
+        }
+    }
+
+    #[test]
+    fn dfs_ungraph() {
+        // complete undirected graph with 5 nodes
+        let mut graph = UnListGraph::with_capacity(5, 10);
+        for _ in 0..5 {
+            graph.insert_node(());
+        }
+        for i in 0..5 {
+            for j in (i+1)..5 {
+                graph.insert_edge(i, j, ());
+            }
+        }
+
+        let expected_parents = HashMap::from([(0, 2), (1, 0), (3, 1), (4, 3)]);
+        for (parent_edge, node) in dfs(&graph, 2) {
+            match parent_edge {
+                Some(edge) => {
+                    let id = node.id();
+                    let expected_parent_id = *expected_parents
+                        .get(&id)
+                        .expect("expected parents map should contain node id");
+                    assert_eq!(
+                        edge.other(id),
+                        expected_parent_id,
+                        "parent of {} should be {} but is {}",
+                        id,
+                        expected_parent_id,
+                        edge.other(id)
+                    )
+                }
+                _ => assert!(!expected_parents.contains_key(&node.id())),
+            }
         }
     }
 }
